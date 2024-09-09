52a92ba25503a6735d741319a5226b06515b02e1
Isaac Wong <isaacwong@Isaacs-MacBook-Air.local>
2024-09-09 17:02:56 +0200

    edit docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,19 @@ release = 'v0.3.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'numpydoc',
+    'nbsphinx',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.autosectionlabel',
+    'sphinx_tabs.tabs',
+    "sphinx.ext.linkcode",
+    'myst_parser'
+]
+autosummary_generate = True
+
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -26,3 +38,40 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+
+def linkcode_resolve(domain, info):
+    """
+    Adapted from https://github.com/aaugustin/websockets/blob/8e1628a14e0dd2ca98871c7500484b5d42d16b67/docs/conf.py
+    """
+    if domain != 'py':
+        return None
+    if not info['module']:
+        return None
+
+    try:
+        mod = importlib.import_module(info["module"])
+        if "." in info["fullname"]:
+            objname, attrname = info["fullname"].split(".")
+            obj = getattr(mod, objname)
+            try:
+                # object is a method of a class
+                obj = getattr(obj, attrname)
+            except AttributeError:
+                # object is an attribute of a class
+                return None
+        else:
+            obj = getattr(mod, info["fullname"])
+
+        try:
+            file = inspect.getsourcefile(obj)
+            lines = inspect.getsourcelines(obj)
+        except TypeError:
+            # e.g. object is a typing.Union
+            return None
+        file = f"{project}/{''.join(file.split(f'{project}/')[1:])}"
+        start, end = lines[1], lines[1] + len(lines[0]) - 1
+    except Exception:
+        return
+
+    return f"{GITURL}/-/tree/{GITHASH}/{file}#L{start}-L{end}"
--- docs/index.rst
+++ docs/index.rst
@@ -3,15 +3,25 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-nullcal documentation
+Welcome to nullcal's documentation!
 =====================
 
-Add your content using ``reStructuredText`` syntax. See the
-`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
-documentation for details.
-
+.. automodule:: nullcal
+    :members:
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
+
+API:
+----
+
+.. autosummary::
+   :toctree: api
+   :template: custom-module-template.rst
+   :caption: API:
+   :recursive:
+
+    likelihood
+    recalibration
