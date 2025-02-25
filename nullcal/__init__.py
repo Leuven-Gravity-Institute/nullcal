deb25b7c039aea2a15b121a425a7fb3adf6b2121
Isaac Wong <chun-fung.wong@tgfli-s0.phy.cuhk.edu.hk>
2025-02-26 00:41:54 +0800

    clean up the code

--- nullcal/__init__.py
+++ nullcal/__init__.py
@@ -1,1 +1,13 @@
+from . import detector
+from . import likelihood
+from . import utils
+from .utils import logger
 from ._version import __version__
+
+
+def get_version_information():
+    return __version__
+
+
+def log_version_information():
+    logger.info(f"Running nullcal: {__version__}")
