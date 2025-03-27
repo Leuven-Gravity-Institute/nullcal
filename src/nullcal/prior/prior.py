c6f1f3870f3e66e5c298780cfecaa1fc0a1f7d34
Isaac C.F. Wong <u0168747@fys-s-dep-clr01.fysad.fys.kuleuven.be>
2025-03-27 16:02:18 +0100

    fix missing bracket when calling super

--- src/nullcal/prior/prior.py
+++ src/nullcal/prior/prior.py
@@ -11,7 +11,7 @@ class CalibrationPriorDict(BilbyCalibrationPriorDict):
             dictionary (dict, optional): See superclass. Defaults to None.
             filename (str, optional): See superclass. Defaults to None.
         """
-        super.__init__(dictionary=dictionary, filename=filename)
+        super().__init__(dictionary=dictionary, filename=filename)
 
     def validate_prior(self, duration, minimum_frequency, N=1000, error=True, warning=False):
         """This is a placeholder method to bypass the checking in bilby_pipe.
